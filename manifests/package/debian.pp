006bb54bc117db9099867f491327b3b55ed9b13c
Michał Czeraszkiewicz <czerasz@gmail.com>
2012-06-24 01:53:43 +0200

    Changed the debian package manifest to solve the issue https://github.com/jfryman/puppet-nginx/issues/3
    	modified:   manifests/package/debian.pp

--- manifests/package/debian.pp
+++ manifests/package/debian.pp
@@ -14,7 +14,28 @@
 #
 # This class file is not called directly
 class nginx::package::debian {
-	package { 'nginx':
-		ensure => present,
-	}
+	if ( ($operatingsystem == "Ubuntu") and ($operatingsystemrelease == 10.04) ) {
+
+    # Patch taken from: http://stackoverflow.com/questions/7209489/how-to-work-with-puppet-dependencies-when-installing-nginx-1-0-5-on-ubuntu-11-04
+    
+    package { "nginx":
+	    ensure => present,
+	    require => Exec["nginx_repository"],
+	  }
+
+	  package { "python-software-properties":
+	    ensure => installed,
+	  }
+
+		exec { "add-apt-repository ppa:nginx/stable && apt-get update":
+			require => Package["python-software-properties"],
+		  alias => "nginx_repository",
+		  path => "/usr/bin",
+		  creates => "/etc/apt/sources.list.d/nginx-stable-natty.list",
+		}
+  } else {
+		package { 'nginx':
+			ensure => present,
+		}	
+  }
 }
